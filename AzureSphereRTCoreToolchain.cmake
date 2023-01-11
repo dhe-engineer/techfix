9fa9e7d190382f3848e5e8b787c0cb5f27f468eb
Dheeraj Ramakrishnan <rdheeraj@microsoft.com>
2023-01-11 18:35:59 +0000

    Common code

---
+++ AzureSphereRTCoreToolchain.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(AS_INT_APP_TYPE "RTApp" CACHE INTERNAL "Type of application (\"HLApp\" or \"RTApp\")")
---
+++ AzureSphereToolchain.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(AS_INT_APP_TYPE "HLApp" CACHE INTERNAL "Type of application (\"HLApp\" or \"RTApp\")")
+
